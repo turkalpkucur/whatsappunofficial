a012627d76bc6e8abdcc35a06b0af0ef78b4e11a
TÜRKALP KUCUR <turkalp.kucur@gmail.com>
2026-02-12 02:48:51 +0300

    güncelleme
    güncelleme

---
+++ frontend/app/page.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { Button, Col, Container, Row, Card } from "react-bootstrap";
+import { useState } from "react";
+import Form from "react-bootstrap/Form";
+
+export default function Home() {
+  const [text, setText] = useState("");
+
+  const handleSend = async () => {
+    await fetch("http://localhost:3001/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        number: "38977863796",
+        message: text,
+      }),
+    });
+  };
+
+  return (
+    <div style={{ backgroundColor: "#f4f6f9", minHeight: "100vh" }}>
+      <Container className="d-flex justify-content-center   min-vh-100">
+        <Row className="w-100 justify-content-center">
+          <Col xs={12} md={6} lg={4}>
+            <Card className="shadow-lg border-0 rounded-4">
+              <Card.Body className="p-4">
+                <h4 className="text-center mb-4 fw-bold">
+                  Mesaj Gönder
+                </h4>
+
+                <Form>
+                  <Form.Group className="mb-3">
+                    <Form.Label>Mesaj</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Mesaj yazın..."
+                      value={text}
+                      onChange={(e) => setText(e.target.value)}
+                    />
+                  </Form.Group>
+
+                  <Button
+                    variant="primary"
+                    className="w-100 rounded-3"
+                    onClick={handleSend}
+                  >
+                    Gönder
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
